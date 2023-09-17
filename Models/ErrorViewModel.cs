c2397ce257c559616c89c59ad7d789ce81f54538
tiagovattos <tiagovattosz@gmail.com>
2023-09-17 16:28:59 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace atendimento_aluno.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
